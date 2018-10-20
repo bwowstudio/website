4386287d78ae104d3f61e0229527c7ff00210993
Manuel <manueldelpinolucena@gmail.com>
2018-10-20 18:42:03 +0200

    number component fixed

--- src/app/shared/controls/controls.component.ts
+++ src/app/shared/controls/controls.component.ts
@@ -8,28 +8,24 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class ControlsComponent implements OnInit {
   @Output() page: EventEmitter<any> = new EventEmitter();
   number = 1;
-  numberSrc = `assets/images/numbers/${this.number}.png`;
   constructor() { }
 
   ngOnInit() {
     const number = localStorage.getItem('bwowPage');
     if (number) {
       this.number = Number(number);
-      this.numberSrc = `assets/images/numbers/${this.number}.png`;
     }
   }
   menuUp() {
     if (this.number < 5) {
       this.number += 1;
     }
-    this.numberSrc = `assets/images/numbers/${this.number}.png`;
     this.page.emit(this.number);
   }
   menuDown() {
     if (this.number > 1) {
       this.number -= 1;
     }
-    this.numberSrc = `assets/images/numbers/${this.number}.png`;
     this.page.emit(this.number);
   }
 
