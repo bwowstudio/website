f06fd1d8fa01477d3c2e606df99af045aa338b52
manueldelpinolucena <manueldelpinolucena@gmail.com>
2018-11-11 18:14:20 +0100

    footer done

--- src/app/components/contact/contact-mobile/contact-mobile.component.ts
+++ src/app/components/contact/contact-mobile/contact-mobile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language.service';
+import { Router } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-contact-mobile',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-mobile.component.scss']
 })
 export class ContactMobileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  placeholders: Array<string>;
+  constructor(
+    public translateService: TranslateService,
+    public languageService: LanguageService,
+    public router: Router
+  ) {
+    this.translateService.use(this.languageService.language);
+    this.languageService.langUpdated.subscribe(e => {
+      this.translateService.use(e);
+    });
   }
-
+  ngOnInit() {
+    forkJoin([
+      this.translateService.get(`CONTACT.placeholder1`),
+      this.translateService.get(`CONTACT.placeholder2`),
+      this.translateService.get(`CONTACT.placeholder3`),
+      this.translateService.get(`CONTACT.placeholder4`),
+      this.translateService.get(`CONTACT.placeholder5`),
+      this.translateService.get(`CONTACT.placeholder6`),
+      this.translateService.get(`CONTACT.placeholder7`),
+      this.translateService.get(`CONTACT.placeholder8`)
+    ]).subscribe(results => {
+      this.placeholders = results;
+  });
+  }
 }
