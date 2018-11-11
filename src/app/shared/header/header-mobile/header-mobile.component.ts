77c5c86b69b5b30bedc20ae4775419cd50a47445
manueldelpinolucena <manueldelpinolucena@gmail.com>
2018-11-11 18:21:22 +0100

    language active

--- src/app/shared/header/header-mobile/header-mobile.component.ts
+++ src/app/shared/header/header-mobile/header-mobile.component.ts
@@ -9,6 +9,8 @@ import { LanguageService } from 'src/app/services/language.service';
 })
 export class HeaderMobileComponent implements OnInit {
   isMenuHidden =  true;
+  langEs = true;
+  langEn = false;
   constructor(
     public translateService: TranslateService,
     public languageService: LanguageService
@@ -17,6 +19,19 @@ export class HeaderMobileComponent implements OnInit {
     this.languageService.langUpdated.subscribe(e => {
       this.translateService.use(e);
     });
+  }
+  setLanguage(language) {
+    this.translateService.use(language);
+    switch (language) {
+      case 'es':
+        this.langEs = true;
+        this.langEn = false;
+        break;
+      case 'en':
+        this.langEs = false;
+        this.langEn = true;
+        break;
+    }
   }
   menuHandler() {
     this.isMenuHidden = !this.isMenuHidden;
