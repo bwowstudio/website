9ab09f556d5ff7634a947da5a3162969189a3337
manueldelpinolucena <manueldelpinolucena@gmail.com>
2018-11-13 19:53:47 +0100

    prueba mobile

--- src/app/components/business/business-mobile/business-mobile.component.ts
+++ src/app/components/business/business-mobile/business-mobile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-business-mobile',
@@ -7,8 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinessMobileComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    public translateService: TranslateService,
+    public languageService: LanguageService,
+    public router: Router
+  ) {
+    this.translateService.use(this.languageService.language);
+    this.languageService.langUpdated.subscribe(e => {
+      this.translateService.use(e);
+    });
+  }
   ngOnInit() {
   }
 
